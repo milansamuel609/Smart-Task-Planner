8f999aa455c16ceac49bd54369ca9c20858fc046
Milan P Samuel <75712741+milansamuel609@users.noreply.github.com>
2025-10-15 09:36:46 +0530

    Update SmarttaskplannerApplication.java

--- Backend/smarttaskplanner/src/main/java/com/milan/smarttaskplanner/SmarttaskplannerApplication.java
+++ Backend/smarttaskplanner/src/main/java/com/milan/smarttaskplanner/SmarttaskplannerApplication.java
@@ -22,7 +22,7 @@ public class SmarttaskplannerApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "https://smart-task-planner-brown.vercel.app/"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
